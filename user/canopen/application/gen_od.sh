6ada1780255dddc1c080f7d9754f582d5a9fdf10
Lv, Qi <lvsoft@gmail.com>
2023-05-22 08:03:04 +0800

    import canopen editor binary

--- user/canopen/application/gen_od.sh
+++ user/canopen/application/gen_od.sh
@@ -1,1 +1,1 @@
-~/install/CANopenEditor/EDSSharp.exe --type CanOpenNode --infile $1 --outfile OD
+../../../canopen/editor/EDSSharp.exe --type CanOpenNode --infile $1 --outfile OD && dos2unix OD.c && dos2unix OD.h
